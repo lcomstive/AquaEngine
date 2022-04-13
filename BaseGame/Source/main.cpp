62d518d626e7392d1105439cc27710d768ac83a4
Lewis Comstive <lcomstive@hotmail.com>
2022-04-14 01:08:49 +0800

    Added GitHub workflow

--- BaseGame/Source/main.cpp
+++ BaseGame/Source/main.cpp
@@ -1,13 +1,42 @@
 #include <spdlog/spdlog.h>
 #include <AquaEngine/API.hpp>
 
+#include <memory>
+#include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+void InitLogger()
+{
+    auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+#ifndef NDEBUG // Debug mode
+    console_sink->set_level(spdlog::level::debug);
+#else
+    console_sink->set_level(spdlog::level::info);
+#endif
+    console_sink->set_pattern("[%H:%M:%S %z] [%^%L%$] %v");
+
+    auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("log.txt", true);
+    file_sink->set_level(spdlog::level::trace);
+
+    spdlog::logger logger("logger", { console_sink, file_sink });
+    logger.set_level(spdlog::level::debug);
+}
+
 int main()
 {
+	InitLogger();
+
 	spdlog::info("Loading game...");
 
-    spdlog::info(" - {:>8}: {1}", "Engine", AQUA_ENGINE_VERSION_LONG);
-    spdlog::info(" - {:>8}: {1}", "Platform", AquaEngine::PlatformName);
-    spdlog::info(" - {:>8}: v{}.{}.{}", "SPDLog", SPDLOG_VER_MAJOR, SPDLOG_VER_MINOR, SPDLOG_VER_PATCH);
+    spdlog::info("{:>12}: v{}.{}.{}-{} [{}]",
+					"Engine",
+					AQUA_ENGINE_VERSION_MAJOR,
+					AQUA_ENGINE_VERSION_MINOR,
+					AQUA_ENGINE_VERSION_PATCH,
+					AQUA_ENGINE_VERSION_REV,
+					AQUA_ENGINE_VERSION_BRANCH
+				);
+    spdlog::info("{:>12}: {}", "Platform", AquaEngine::PlatformName);
+    spdlog::info("{:>12}: v{}.{}.{}", "SPDLog", SPDLOG_VER_MAJOR, SPDLOG_VER_MINOR, SPDLOG_VER_PATCH);
 
 	return 0;
 }
